171851deb873bb75fb40e5e5cd68b2cc70c825d8
Muhsina-Tasni <tasnimuhsina@gmail.com>
2026-03-12 14:24:22 +0530

    changed auth

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,15 +1,30 @@
 
+// // routes/categoryRoutes.js
+// const express = require("express");
+// const router = express.Router();
+// const auth = require("../middleware/AuthMiddlware");
+// const adminOnly = require("../middleware/adminMiddlware");
+// const { createCategory, getCategories, updateCategory, deleteCategory,getCategoryById } = require("../controllers/categoryController");
+
+// router.post("/", auth, adminOnly, createCategory);
+// router.get("/", getCategories);
+// router.put("/:id", auth, adminOnly, updateCategory);
+// router.delete("/:id", auth, adminOnly, deleteCategory);
+//  router.get("/:id", auth, getCategoryById);
+
+// module.exports = router;
+
 // routes/categoryRoutes.js
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/AuthMiddlware");
-const adminOnly = require("../middleware/adminMiddlware");
+
 const { createCategory, getCategories, updateCategory, deleteCategory,getCategoryById } = require("../controllers/categoryController");
 
-router.post("/", auth, adminOnly, createCategory);
+router.post("/", auth, createCategory);
 router.get("/", getCategories);
-router.put("/:id", auth, adminOnly, updateCategory);
-router.delete("/:id", auth, adminOnly, deleteCategory);
+router.put("/:id", auth,  updateCategory);
+router.delete("/:id", auth, deleteCategory);
  router.get("/:id", auth, getCategoryById);
 
 module.exports = router;
