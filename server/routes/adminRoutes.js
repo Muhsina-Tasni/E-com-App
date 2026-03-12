8c26168d9f1bc2d35682e58cb27d4b0483ccffdd
Muhsina-Tasni <tasnimuhsina@gmail.com>
2026-03-12 14:53:56 +0530

    changed name

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 //adminrouter.js
 const express = require("express");
 const router = express.Router();
-const { getAdminStats } = require("../controllers/temp");
+const { getAdminStats } = require("../controllers/adminController");
 const authMiddleware = require("../middleware/AuthMiddlware"); 
 
 router.get("/stats", authMiddleware, getAdminStats);
