729900a3c1df73db03223696d9d234469629c534
Muhsina-Tasni <tasnimuhsina@gmail.com>
2026-03-12 14:51:19 +0530

    payment changed

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -28,41 +28,41 @@
 
 
 
-const Payment = require("../models/Payment");
+// const Payment = require("../models/Payment");
 
-const createPayment = async (req, res) => {
-  try {
-    const { order_id, paymentMethod, amount, transactionId } = req.body;
+// const createPayment = async (req, res) => {
+//   try {
+//     const { order_id, paymentMethod, amount, transactionId } = req.body;
 
-    // simple validation
-    if (!order_id || !paymentMethod || !amount || !transactionId) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
+//     // simple validation
+//     if (!order_id || !paymentMethod || !amount || !transactionId) {
+//       return res.status(400).json({ message: "All fields are required" });
+//     }
 
-    const payment = await Payment.create({
-      order_id,
-      paymentMethod,
-      amount,
-      status: "success",   // must match schema enum
-      paymentDate: new Date(),
-      transactionId,
-    });
+//     const payment = await Payment.create({
+//       order_id,
+//       paymentMethod,
+//       amount,
+//       status: "success",   // must match schema enum
+//       paymentDate: new Date(),
+//       transactionId,
+//     });
 
-    res.status(201).json({
-      success: true,
-      message: "Payment created successfully",
-      payment,
-    });
+//     res.status(201).json({
+//       success: true,
+//       message: "Payment created successfully",
+//       payment,
+//     });
 
-  } catch (err) {
-    console.error("Payment Error:", err);
-    res.status(500).json({
-      success: false,
-      message: err.message,
-      error: err,
-    });
-  }
-};
+//   } catch (err) {
+//     console.error("Payment Error:", err);
+//     res.status(500).json({
+//       success: false,
+//       message: err.message,
+//       error: err,
+//     });
+//   }
+// };
 
 
-module.exports = {createPayment}
+// module.exports = {createPayment}
