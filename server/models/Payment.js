3c4d52e8e247f8f59f0621d95aedc92013c0d472
Muhsina-Tasni <tasnimuhsina@gmail.com>
2026-03-15 12:06:45 +0530

    fix paymentRoutes router export

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,37 +1,37 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema(
-  {
-    order_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Order", // reference to Orders
-      required: true,
-    },
-    paymentMethod: {
-      type: String,
-      enum: ["card", "upi", "netbanking", "cod", "wallet"], // define allowed methods
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["pending","success" , "failed", "refunded"],
-      default: "pending",
-    },
-    amount: {
-      type: Number,
-      required: true,
-    },
-    paymentDate: {
-      type: Date,
-      default: Date.now,
-    },
-    transactionId: {
-      type: String,
-      unique: true, // transaction id should be unique
-      required: true,
-    },
-  },
-  { timestamps: true } // adds createdAt & updatedAt automatically
-);
+// const paymentSchema = new mongoose.Schema(
+//   {
+//     order_id: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Order", // reference to Orders
+//       required: true,
+//     },
+//     paymentMethod: {
+//       type: String,
+//       enum: ["card", "upi", "netbanking", "cod", "wallet"], // define allowed methods
+//       required: true,
+//     },
+//     status: {
+//       type: String,
+//       enum: ["pending","success" , "failed", "refunded"],
+//       default: "pending",
+//     },
+//     amount: {
+//       type: Number,
+//       required: true,
+//     },
+//     paymentDate: {
+//       type: Date,
+//       default: Date.now,
+//     },
+//     transactionId: {
+//       type: String,
+//       unique: true, // transaction id should be unique
+//       required: true,
+//     },
+//   },
+//   { timestamps: true } // adds createdAt & updatedAt automatically
+// );
 
-module.exports = mongoose.model("Payment", paymentSchema);
+// module.exports = mongoose.model("Payment", paymentSchema);
